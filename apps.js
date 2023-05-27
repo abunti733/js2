3cb2cd6a23d6d97b5e2f547195995d54fbb36ee7
abunti733 <128597637+abunti733@users.noreply.github.com>
2023-05-27 08:57:47 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,22 @@
+// var student={sname:"Jamal Miah",height:"5.11",education:"CSE",hobby:"Cricket"};
+// document.getElementById('print').innerHTML=student.sname+"<br>"+student.height;
+// document.getElementById('print').style.color='red';
+// document.getElementById('print').style.fontSize='40px';
+
+
+var student={sname:"jamal",height:"5.11",education:"BBA",hobby:"cricket",};
+document.getElementById('print').innerHTML=student.sname+"<br>"+student.height;
+document.getElementById('print').style.color='red';
+document.getElementById('print').style.fontSize='40px';
+
+// var sarray=["Ruhi","Rafi","Sapon","Sanu",]
+// sarray.push("Arry");
+// sarray.pop("Arry");
+// document.write(sarray);
+
+// function addStyle(){
+	// document.getElementById('print').style.color='red';
+// }
+// function removeStyle(){
+	// document.getElementById('print').style.color='';
+// }
